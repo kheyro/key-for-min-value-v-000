45f98eee68b164d440ccbbe1c899899d0e1b61b8
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-30 20:18:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,6 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if !defined? last_value
         last_value = value
-        binding.pry
         next
       end
       if value < last_value
