3b5265524e425d384b6c7592a59a83d75877ff96
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-30 20:20:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,12 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
+  last_value = ""
   last_key = ""
 
   if !name_hash.empty?
     name_hash.each do |key, value|
-      if !defined? last_value
+      if last_value == ""
         last_value = value
         last_key = key
         next
